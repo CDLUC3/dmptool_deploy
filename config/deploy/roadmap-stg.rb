29514270efde1bbbdfae480de0ac85d774109330
briley <brian.riley@ucop.edu>
2017-05-09 13:48:22 -0700

    updated location of config repo

--- config/deploy/roadmap-stg.rb
+++ config/deploy/roadmap-stg.rb
@@ -14,7 +14,7 @@ set :deploy_to, '/dmp/apps/roadmap'
 set :share_to, 'dmp/apps/roadmap/shared'
 
 # Define the location of the configuration repo
-set :config_repo, 'git@github.com:cdlib/dmptool_config.git'
+set :config_repo, 'git@github.com:CDLUC3/roadmap_deploy.git'
 set :config_branch, 'roadmap-stage'
 
 # role-based syntax
