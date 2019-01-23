353fe0f424ea306dd3de120f97e1d1f00d58660e
briri <brian.riley@ucop.edu>
2019-01-23 14:13:01 -0800

    updated for prod release

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,13 +25,7 @@ append :linked_files, 'config/database.yml',
                       'config/initializers/recaptcha.rb',
                       'config/initializers/contact_us.rb',
                       'config/initializers/devise.rb',
-                      'config/initializers/wicked_pdf.rb',
-                      'public/eds.html',
-                      'public/fullDiscoFeed.json',
-                      'public/idpselect_config.js',
-                      'public/idpselect.css',
-                      'public/idpselect.js',
-                      'public/localDiscoFeed.json'
+                      'config/initializers/wicked_pdf.rb'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log',
@@ -44,50 +38,10 @@ append :linked_dirs, 'log',
 set :keep_releases, 5
 
 namespace :deploy do
-  # STILL NEEDED FOR PROD
-  #after :deploy, 'cleanup:remove_example_configs'
-
   before :deploy, 'config:install_shared_dir'
+  after :deploy, 'cleanup:remove_example_configs'
   after :deploy, 'cleanup:restart_passenger'
-
-# FOR NEW Roadmap 2.x configuration
-# TODO: Uncomment this for deployments to dmp-dev or roadmap-stg and permenantly once we have
-#       merged the latest changes into dmptool stage and prod
-#  namespace :assets do
-#    before :backup_manifest, 'deploy:create_asset_manifests'
-#  end
-
-#  desc 'Create an empty assets manifest to satisfy Capistrano rails assets gem'
-#  task :create_asset_manifests do
-#    on roles(:app), wait: 1 do
-#      execute "cd #{release_path} && mkdir -p public/assets"
-#      execute "cd #{release_path} && touch public/assets/manifest.json"
-#      execute "cd #{release_path} && touch public/assets/.sprockets-manifest.json"
-#    end
-#  end
 end
-
-#namespace :git do
-  # STILL NEEDED FOR PROD
-  #after :create_release, 'remove_postgres'
-
-  # v2.0
-  #after :create_release, 'create_asset_manifests'
-
-  # STILL NEEDED FOR PROD
-  #desc 'Remove the postgres dependency from the Gemfile'
-  #task :remove_postgres do
-  #  on roles(:app), wait: 1 do
-      # Comment out the Postgres gem
-  #    execute "cd #{release_path} && mv Gemfile Gemfile.bak"
-  #    execute "cd #{release_path} && cat Gemfile.bak | sed 's/gem \\x27pg\\x27/#gem \\x27pg\\x27/' >> Gemfile"
-  #    execute "cd #{release_path} && bundle install"
-
-      # FOR NEW Roadmap 2.x configuration
-      #execute "cd #{release_path} && bundle install --without puma psql thin"
-  #  end
-  #end
-#end
 
 namespace :config do
   desc 'Setup up the config repo as the shared directory'
@@ -100,16 +54,13 @@ namespace :config do
 end
 
 namespace :cleanup do
-  # STILL NEEDED FOR PROD
-  # this prevents examples from being used on the server
-  #desc "Remove all of the example config files"
-  #task :remove_example_configs do
-  #  on roles(:app), wait: 1 do
-  #    execute "rm -f #{release_path}/config/*.yml.sample"
-  #    execute "rm -f #{release_path}/config/initializers/contac_us_example.rb"
-  #    execute "rm -f #{release_path}/config/initializers/*.rb.example"
-  #  end
-  #end
+  desc "Remove all of the example config files"
+  task :remove_example_configs do
+    on roles(:app), wait: 1 do
+      execute "rm -f #{release_path}/config/*.yml.sample"
+      execute "rm -f #{release_path}/config/initializers/*.rb.example"
+    end
+  end
 
   desc 'Restart Phusion Passenger'
   task :restart_passenger do
--- config/deploy/uc3-dmp-prd.rb
+++ config/deploy/uc3-dmp-prd.rb
@@ -1,61 +1,59 @@
 set :application, 'DMPTool'
 set :repo_url, 'https://github.com/CDLUC3/dmptool.git'
 
-# server-based syntax
-# ======================
-# Defines a single server with a list of roles and multiple properties.
-# You can define all roles on a single server, or split them:
 set :server_host, ENV["SERVER_HOST"] || 'uc3-dmp-prd.cdlib.org'
 server fetch(:server_host), user: 'dmp', roles: %w{web app db}
 
-# Default deploy_to directory is /var/www/my_app_name
-set :default_env, { path: "/dmp/local/bin:$PATH" }
 set :deploy_to, '/dmp/apps/dmp'
 set :share_to, 'dmp/apps/dmp/shared'
 
-# Define the location of the configuration repo
-set :config_repo, 'git@github.com:cdlib/dmptool_config.git'
-set :config_branch, 'master'
+# Define the location of the private configuration repo
+set :config_branch, 'uc3-dmp-prd'
+
+set :rails_env, 'production'
+
+
+# PRE VERSION 2.0
+# --------------------------------------------------------
+
+# Default deploy_to directory is /var/www/my_app_name
+#set :default_env, { path: "/dmp/local/bin:$PATH" }
 
 # Copy over the homepage images
-append :linked_dirs, 'lib/assets/images/homepage'
+#append :linked_dirs, 'lib/assets/images/homepage'
 
 # Default environments to skip
-set :bundle_without, %w{development test}.join(' ')
+#set :bundle_without, %w{development test}.join(' ')
 
-# We are running stage as if it were a prod environment right now
-set :rails_env, 'production'
-set :passenger_restart, "cd /apps/dmp/init.d && ./passenger-dmp.dmp restart"
+#namespace :git do
+#  after :create_release, 'npm_install'
 
-namespace :git do
-  after :create_release, 'npm_install'
+#  desc 'Install all of the resources managed by NPM'
+#  task :npm_install do
+#    on roles(:app), wait: 1 do
+#      execute "cd #{release_path}/lib/assets && npm install && cd .."
+#    end
+#  end
+#end
 
-  desc 'Install all of the resources managed by NPM'
-  task :npm_install do
-    on roles(:app), wait: 1 do
-      execute "cd #{release_path}/lib/assets && npm install && cd .."
-    end
-  end
-end
+#namespace :deploy do
+#  after :cleanup, 'webpack_bundle'
+#  after :cleanup, 'move_compiled_jpegs'
 
-namespace :deploy do
-  after :cleanup, 'webpack_bundle'
-  after :cleanup, 'move_compiled_jpegs'
+#  # Update this for Roadmap v2.x
+#  desc 'Bundle the Webpack managed assets'
+#  task :webpack_bundle do
+#    on roles(:app), wait: 1 do
+#      execute "cd #{release_path}/lib/assets && npm run bundle -- -p"
+#    end
+#  end
 
-  # Update this for Roadmap v2.x
-  desc 'Bundle the Webpack managed assets'
-  task :webpack_bundle do
-    on roles(:app), wait: 1 do
-      execute "cd #{release_path}/lib/assets && npm run bundle -- -p"
-    end
-  end
-
-  # Webpack will compile and place SASS requests for `background: url('file.jpg')` into the root public/ dir
-  # the compiled CSS though will look for these files in public/stylesheets so we need to move them over
-  desc 'Transfer compiled JPEGs over to the public/stylesheets dir'
-  task :move_compiled_jpegs do
-    on roles(:app), wait: 10 do
-      execute "cd #{release_path}/public && ls -1 | egrep '[a-zA-Z0-9]{32}\\.[jpg|png]' | xargs mv -t #{release_path}/public/stylesheets"
-    end
-  end
-end
+#  # Webpack will compile and place SASS requests for `background: url('file.jpg')` into the root public/ dir
+#  # the compiled CSS though will look for these files in public/stylesheets so we need to move them over
+#  desc 'Transfer compiled JPEGs over to the public/stylesheets dir'
+#  task :move_compiled_jpegs do
+#    on roles(:app), wait: 10 do
+#      execute "cd #{release_path}/public && ls -1 | egrep '[a-zA-Z0-9]{32}\\.[jpg|png]' | xargs mv -t #{release_path}/public/stylesheets"
+#    end
+#  end
+#end
